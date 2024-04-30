2aaf107b221e653badc149bb8d7478cc4c570eee
J.D. Lowe <jdlowe.dev@gmail.com>
2024-04-30 19:35:17 -0400

    Starting context work

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,51 +1,54 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import { StartScreen } from './pages';
+import { AppContextProvider } from './appContext';
 import './App.css';
 
 function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate fart</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="Hello">
+				<img width="200" alt="icon" src={icon} />
+			</div>
+			<h1>electron-react-boilerplate fart</h1>
+			<div className="Hello">
+				<a
+					href="https://electron-react-boilerplate.js.org/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button type="button">
+						<span role="img" aria-label="books">
+							📚
+						</span>
+						Read our docs
+					</button>
+				</a>
+				<a
+					href="https://github.com/sponsors/electron-react-boilerplate"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button type="button">
+						<span role="img" aria-label="folded hands">
+							🙏
+						</span>
+						Donate
+					</button>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StartScreen />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<AppContextProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<StartScreen />} />
+				</Routes>
+			</Router>
+		</AppContextProvider>
+	);
 }
---
+++ src/renderer/appContext.ts
@@ -0,0 +1,16 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+interface AppContextProviderProps {
+    children?: React.ReactNode | React.ReactNode[]
+}
+
+const AppContext = createContext({});
+
+export const AppContextProvider:React.FC<AppContextProviderProps> = ({children}) => {
+    return <AppContext.Provider value={}>{children}</AppContext.Provider>
+};
+
+export const useAppContext = () => {
+    const value = useContext(AppContext);
+    return value;
+}
