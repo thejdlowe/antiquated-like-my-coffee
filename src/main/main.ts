93e50ad208e87591ee59f9dc3cf92c0bd609f89d
J.D. Lowe <jdlowe.dev@gmail.com>
2024-04-30 18:57:59 -0400

    Redoing buttons

--- src/main/main.ts
+++ src/main/main.ts
@@ -17,154 +17,161 @@ import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
 const DEVICE_INFO = {
-  vendorId: 1118,
-  productId: 672,
-  interfaceId: 0,
-  endpointId: 0,
+	vendorId: 1118,
+	productId: 672,
+	interfaceId: 0,
+	endpointId: 0,
 };
 
 class AppUpdater {
-  constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
+	constructor() {
+		log.transports.file.level = 'info';
+		autoUpdater.logger = log;
+		autoUpdater.checkForUpdatesAndNotify();
+	}
 }
 
 let mainWindow: BrowserWindow | null = null;
 
 const initiateIRReceiver = async () => {
-  const legacyDevice = findByIds(DEVICE_INFO.vendorId, DEVICE_INFO.productId);
-  let canAccept = false;
-  let lastValue = '';
-  if (legacyDevice) {
-    legacyDevice.open();
-    const legacyInterface = legacyDevice.interfaces[DEVICE_INFO.interfaceId];
+	const legacyDevice = findByIds(DEVICE_INFO.vendorId, DEVICE_INFO.productId);
+	let canAccept = false;
+	let lastValue = '';
+	if (legacyDevice) {
+		legacyDevice.open();
+		const legacyInterface =
+			legacyDevice.interfaces[DEVICE_INFO.interfaceId];
 
-    if (legacyInterface.isKernelDriverActive()) {
-      legacyInterface.detachKernelDriver();
-    }
+		if (legacyInterface.isKernelDriverActive()) {
+			legacyInterface.detachKernelDriver();
+		}
 
-    const inEndpoint = legacyInterface.endpoints[DEVICE_INFO.endpointId];
+		const inEndpoint = legacyInterface.endpoints[DEVICE_INFO.endpointId];
 
-    inEndpoint.on('data', (usbEvent) => {
-      const dataView = new Uint8Array(usbEvent);
-      const whichController = dataView[2];
-      const decoded = `${dataView[0]}:${dataView[1]}:${dataView[2]}:${dataView[3]}:${dataView[4]}`;
-      console.log(decoded);
-      if (lastValue !== decoded) {
-        if (whichController === 2) {
-          canAccept = true;
-          mainWindow.webContents.send('button-update', 'reset');
-          lastValue = decoded;
-        } else if (canAccept === true) {
-          canAccept = false;
-          let whatToSend = 'broken';
-          if (whichController === 0) whatToSend = 'P1';
-          else if (whichController === 1) whatToSend = 'P2';
-          else if (whichController === 3) whatToSend = 'P3';
-          mainWindow.webContents.send('button-update', whatToSend);
-          lastValue = decoded;
-        }
-      }
-    });
+		inEndpoint.on('data', (usbEvent) => {
+			const dataView = new Uint8Array(usbEvent);
+			const whichController = dataView[2];
+			const buttonsPressed = dataView[4];
+			const altButtonsPressed = dataView[3];
+			const startButton = !!(altButtonsPressed & 0x10);
+			const backButton = !!(altButtonsPressed & 0x20);
+			const XboxButton = !!(buttonsPressed & 0x04);
+			const bigButton = !!(buttonsPressed & 0x08);
+			const AButton = !!(buttonsPressed & 0x10);
+			const BButton = !!(buttonsPressed & 0x20);
+			const XButton = !!(buttonsPressed & 0x40);
+			const YButton = !!(buttonsPressed & 0x80);
+			mainWindow?.webContents.send('button-pressed', {
+				whichController,
+				startButton,
+				backButton,
+				XboxButton,
+				bigButton,
+				AButton,
+				BButton,
+				XButton,
+				YButton,
+			});
+		});
 
-    inEndpoint.on('error', (err) => {
-      console.log('Error', err);
-    });
+		inEndpoint.on('error', (err) => {
+			console.log('Error', err);
+		});
 
-    inEndpoint.startPoll();
-  }
+		inEndpoint.startPoll();
+	}
 };
 
 ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
+	const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
+	console.log(msgTemplate(arg));
+	event.reply('ipc-example', msgTemplate('pong'));
 });
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
+	const sourceMapSupport = require('source-map-support');
+	sourceMapSupport.install();
 }
 
 const isDebug =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+	process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
 if (isDebug) {
-  require('electron-debug')();
+	require('electron-debug')();
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
+	const installer = require('electron-devtools-installer');
+	const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+	const extensions = ['REACT_DEVELOPER_TOOLS'];
 
-  return installer
-    .default(
-      extensions.map((name) => installer[name]),
-      forceDownload,
-    )
-    .catch(console.log);
+	return installer
+		.default(
+			extensions.map((name) => installer[name]),
+			forceDownload,
+		)
+		.catch(console.log);
 };
 
 const createWindow = async () => {
-  if (isDebug) {
-    //await installExtensions();
-  }
+	if (isDebug) {
+		//await installExtensions();
+	}
 
-  const RESOURCES_PATH = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
+	const RESOURCES_PATH = app.isPackaged
+		? path.join(process.resourcesPath, 'assets')
+		: path.join(__dirname, '../../assets');
 
-  const getAssetPath = (...paths: string[]): string => {
-    return path.join(RESOURCES_PATH, ...paths);
-  };
+	const getAssetPath = (...paths: string[]): string => {
+		return path.join(RESOURCES_PATH, ...paths);
+	};
 
-  mainWindow = new BrowserWindow({
-    show: false,
-    width: 1024,
-    height: 728,
-    icon: getAssetPath('icon.png'),
-    webPreferences: {
-      preload: app.isPackaged
-        ? path.join(__dirname, 'preload.js')
-        : path.join(__dirname, '../../.erb/dll/preload.js'),
-    },
-  });
+	mainWindow = new BrowserWindow({
+		show: false,
+		width: 1024,
+		height: 728,
+		icon: getAssetPath('icon.png'),
+		webPreferences: {
+			preload: app.isPackaged
+				? path.join(__dirname, 'preload.js')
+				: path.join(__dirname, '../../.erb/dll/preload.js'),
+		},
+	});
 
-  mainWindow.loadURL(resolveHtmlPath('index.html'));
+	mainWindow.loadURL(resolveHtmlPath('index.html'));
 
-  mainWindow.on('ready-to-show', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.webContents.setDevToolsWebContents(mainWindow.webContents);
-    }
-  });
+	mainWindow.on('ready-to-show', () => {
+		if (!mainWindow) {
+			throw new Error('"mainWindow" is not defined');
+		}
+		if (process.env.START_MINIMIZED) {
+			mainWindow.minimize();
+		} else {
+			mainWindow.show();
+			mainWindow.webContents.setDevToolsWebContents(
+				mainWindow.webContents,
+			);
+		}
+	});
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
 
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+	const menuBuilder = new MenuBuilder(mainWindow);
+	menuBuilder.buildMenu();
 
-  // Open urls in the user's browser
-  mainWindow.webContents.setWindowOpenHandler((edata) => {
-    shell.openExternal(edata.url);
-    return { action: 'deny' };
-  });
+	// Open urls in the user's browser
+	mainWindow.webContents.setWindowOpenHandler((edata) => {
+		shell.openExternal(edata.url);
+		return { action: 'deny' };
+	});
 
-  initiateIRReceiver();
+	initiateIRReceiver();
 
-  // Remove this if your app does not use auto updates
-  // eslint-disable-next-line
-  // new AppUpdater();
+	// Remove this if your app does not use auto updates
+	// eslint-disable-next-line
+	// new AppUpdater();
 };
 
 /**
@@ -172,21 +179,20 @@ const createWindow = async () => {
  */
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	// Respect the OSX convention of having the application in memory even
+	// after all windows have been closed
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-app
-  .whenReady()
-  .then(() => {
-    createWindow();
-    app.on('activate', () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
-    });
-  })
-  .catch(console.log);
+app.whenReady()
+	.then(() => {
+		createWindow();
+		app.on('activate', () => {
+			// On macOS it's common to re-create a window in the app when the
+			// dock icon is clicked and there are no other windows open.
+			if (mainWindow === null) createWindow();
+		});
+	})
+	.catch(console.log);
