d6030b9dd1bdc7fa4f9fa331288575d2a471c48f
J.D. Lowe <jdlowe.dev@gmail.com>
2024-04-30 19:05:58 -0400

    Typo!

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -5,29 +5,31 @@ import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 export type Channels = 'ipc-example';
 
 const electronHandler = {
-  ipcRenderer: {
-    sendMessage(channel: Channels, ...args: unknown[]) {
-      ipcRenderer.send(channel, ...args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
+	ipcRenderer: {
+		sendMessage(channel: Channels, ...args: unknown[]) {
+			ipcRenderer.send(channel, ...args);
+		},
+		on(channel: Channels, func: (...args: unknown[]) => void) {
+			const subscription = (
+				_event: IpcRendererEvent,
+				...args: unknown[]
+			) => func(...args);
+			ipcRenderer.on(channel, subscription);
 
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-  },
+			return () => {
+				ipcRenderer.removeListener(channel, subscription);
+			};
+		},
+		once(channel: Channels, func: (...args: unknown[]) => void) {
+			ipcRenderer.once(channel, (_event, ...args) => func(...args));
+		},
+	},
 };
 
 contextBridge.exposeInMainWorld('electron', electronHandler);
 contextBridge.exposeInMainWorld('electronAPI', {
-  onButtonUpdate: (callback) =>
-    ipcRenderer.on('button-update', (_event, value) => callback(value)),
+	onButtonUpdate: (callback) =>
+		ipcRenderer.on('button-pressed', (_event, value) => callback(value)),
 });
 
 export type ElectronHandler = typeof electronHandler;
