98f6e731730f60eaa7f2e94da422716d12e6876b
J.D. Lowe <jdlowe.dev@gmail.com>
2024-05-01 19:29:19 -0400

    Starting to add State

--- src/renderer/appContext.tsx
+++ src/renderer/appContext.tsx
@@ -1,17 +1,47 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 interface AppContextProviderProps {
-    children?: React.ReactNode | React.ReactNode[]
+	children?: React.ReactNode | React.ReactNode[];
 }
 
 const AppContext = createContext({});
 
-export const AppContextProvider:React.FC<AppContextProviderProps> = ({children}) => {
+type ShowStates = 'beginning' | 'active' | 'shutdown';
 
-    return (<AppContext.Provider value={{}}>{children}</AppContext.Provider>)
+export const AppContextProvider: React.FC<AppContextProviderProps> = ({
+	children,
+}) => {
+	const [showState, setShowState] = useState<ShowStates>('beginning');
+	const [buttonPressed, setButtonPressed] = useState({});
+
+	useEffect(() => {
+		window.electronAPI.onButtonUpdate((value) => {
+			// if (value === "ready") {
+			// 	document.querySelectorAll("#P1, #P2, #P3").forEach((el) => {
+			// 		el.style.backgroundColor = "white";
+			// 	});
+			// 	document.querySelector("#rawValue").innerHTML = "Ready";
+			// } else if (["P1", "P2", "P3"].includes(value)) {
+			// 	const el = document.querySelector(`#${value}`);
+			// 	el.style.backgroundColor = el.dataset.background;
+			// 	document.querySelector("#rawValue").innerHTML = "Not Ready";
+			// }
+
+			setButtonPressed(value);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(buttonPressed);
+	}, [buttonPressed, showState]);
+	return (
+		<AppContext.Provider value={{ showState, setShowState }}>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export const useAppContext = () => {
-    const value = useContext(AppContext);
-    return value;
-}
+	const value = useContext(AppContext);
+	return value;
+};
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -11,18 +11,3 @@ window.electron.ipcRenderer.once('ipc-example', (arg) => {
 	console.log(arg);
 });
 window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
-
-window.electronAPI.onButtonUpdate((value) => {
-	// if (value === "ready") {
-	// 	document.querySelectorAll("#P1, #P2, #P3").forEach((el) => {
-	// 		el.style.backgroundColor = "white";
-	// 	});
-	// 	document.querySelector("#rawValue").innerHTML = "Ready";
-	// } else if (["P1", "P2", "P3"].includes(value)) {
-	// 	const el = document.querySelector(`#${value}`);
-	// 	el.style.backgroundColor = el.dataset.background;
-	// 	document.querySelector("#rawValue").innerHTML = "Not Ready";
-	// }
-
-	console.log(value);
-});
