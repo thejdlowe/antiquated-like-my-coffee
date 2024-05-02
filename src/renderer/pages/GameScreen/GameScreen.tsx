885b12fe0668c8e9b69d7567bf4a1f37d9b77a7e
J.D. Lowe <jdlowe.dev@gmail.com>
2024-05-02 15:18:36 -0400

    starting the timer code

--- src/renderer/pages/GameScreen/GameScreen.tsx
+++ src/renderer/pages/GameScreen/GameScreen.tsx
@@ -1,7 +1,21 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 
 export const GameScreen = () => {
+	const timerRef = useRef(null);
+	const [timeRemaining, setTimeRemaining] = useState(0);
+
+	const parseTimer = useCallback(() => {
+		const minutes = Math.floor(timeRemaining / 60);
+		const seconds = Math.floor(timeRemaining % 60);
+		return [(minutes > 9 ? minutes : "0" + minutes), (seconds > 9 ? seconds : "0" + seconds)].join(":");
+	}, [timeRemaining]);
+
+	const updateTimer = () => {
+
+	};
+	useEffect(() => {
+
+	}, []);
 	return (
 		<>
 			<div>WE ON GAME SCREEN</div>
