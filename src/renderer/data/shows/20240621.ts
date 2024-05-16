08d7067d3a3ee28b7492059b10552386e053e8d5
J.D. Lowe <jdlowe.dev@gmail.com>
2024-05-15 22:49:45 -0400

    Show data updated

--- src/renderer/data/shows/20240621.ts
+++ src/renderer/data/shows/20240621.ts
@@ -1,21 +1,25 @@
 import { ShowType } from '../show.type';
 import { playerImages } from '../../media';
+
 export const show: ShowType = {
 	Round1: [
 		{
-			displayName: 'Anthony',
-			fullName: 'Anthony Windsor',
-			imagePath: playerImages.Alex,
-		},
-		{
 			displayName: 'Chris F.',
 			fullName: 'Chris Fay',
+			imagePath: playerImages.Alex,
+			pronouns: 'He/Him',
+		},
+		{
+			displayName: 'Chris H.',
+			fullName: 'Chris Heiberger',
 			imagePath: playerImages.BC,
+			pronouns: 'He/Him',
 		},
 		{
 			displayName: 'Heather',
 			fullName: 'Heather Scott',
 			imagePath: playerImages.Bridjet,
+			pronouns: 'She/Her',
 		},
 	],
 	Round2: [
@@ -23,16 +27,19 @@ export const show: ShowType = {
 			displayName: 'Q',
 			fullName: 'Chakra Quan',
 			imagePath: playerImages.Erik,
+			pronouns: 'She/Her',
 		},
 		{
 			displayName: 'Motts',
 			fullName: 'The Motts',
 			imagePath: playerImages.Jen,
+			pronouns: 'He/Him',
 		},
 		{
 			displayName: 'Kaley',
 			fullName: 'Kaley Michelle',
 			imagePath: playerImages.Jonathan,
+			pronouns: 'She/Her',
 		},
 	],
 	Round3: [
@@ -40,16 +47,19 @@ export const show: ShowType = {
 			displayName: 'Rob',
 			fullName: 'Rob Moore',
 			imagePath: playerImages.Mark,
+			pronouns: 'He/Him',
 		},
 		{
-			displayName: 'Chris H.',
-			fullName: 'Chris Heiberger',
+			displayName: 'Lord Anthony',
+			fullName: 'Lord Anthony Windsor II',
 			imagePath: playerImages.Mike,
+			pronouns: 'He/Him',
 		},
 		{
 			displayName: 'Hannah',
 			fullName: 'Hannah Romes',
 			imagePath: playerImages.Terrance,
+			pronouns: 'She/Her',
 		},
 	],
 };
