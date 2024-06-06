d39669560616ef2139d14fbfcaa97711ce308b51
J.D. Lowe <jdlowe.dev@gmail.com>
2024-06-05 23:36:56 -0400

    Undo webpack?

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -15,11 +15,6 @@ const configuration: webpack.Configuration = {
 	module: {
 		rules: [
 			{
-				test: /\.wav$|\.mp3$/,
-				exclude: /node_modules/,
-				loader: 'file-loader',
-			},
-			{
 				test: /\.[jt]sx?$/,
 				exclude: /node_modules/,
 				use: {
