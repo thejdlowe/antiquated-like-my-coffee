62270b7ecb4838bea8005dcf72c0707845f9e2f3
J.D. Lowe <jdlowe.dev@gmail.com>
2024-06-05 22:38:33 -0400

    Fine, go into a hook file

--- src/renderer/media/index.ts
+++ src/renderer/media/index.ts
@@ -1,5 +1,4 @@
-import useSound from 'use-sound';
-import { useRef, useEffect, useState } from 'react';
+
 import Anthony from './Anthony.png';
 import Chakra from './Chakra.png';
 import ChrisH from './Chris H.png';
@@ -29,53 +28,4 @@ export const playerImages = {
 
 export const logo = Logo;
 
-export const useGameSounds = () => {
-	// const gameSoundRef = useRef({
-	// 	playAllSounds: () => {},
-	// 	playerSounds: [() => {}],
-	// });
-	const [playAllSounds, setPlayAllSounds] = useState(() => () => {});
-	const [playerSounds, setPlayerSounds] = useState([() => {}]);
-	//const { playAllSounds, playerSounds } = gameSoundRef.current;
-	useEffect(() => {
-		const [play] = useSound('./LMCsoundeffects.mp3', {
-			sprite: {
-				amongus: [0, 3356],
-				fortnite: [6468, 6557 - 6468],
-				gameboy: [7556, 8962 - 7556],
-				homerun: [10275, 11372 - 10275],
-				icq: [12948, 14081 - 12948],
-				mariopause: [17188, 18185 - 17188],
-				metalgear: [19859, 21583 - 19859],
-				mariopowerup: [22427, 23697 - 22427],
-				konami: [24445, 25216 - 24445],
-				wheeloffortune: [26008, 27233 - 26008],
-				zelda: [29336, 31150 - 29336],
-			},
-		});
-
-		const playAllSounds = () => {
-			play();
-		};
-
-		const playerSounds = [
-			() => play({ id: 'amongus' }),
-			() => play({ id: 'fortnite' }),
-			() => play({ id: 'gameboy' }),
-			() => play({ id: 'homerun' }),
-			() => play({ id: 'icq' }),
-			() => play({ id: 'mariopause' }),
-			() => play({ id: 'metalgear' }),
-			() => play({ id: 'mariopowerup' }),
-			() => play({ id: 'konami' }),
-			() => play({ id: 'wheeloffortune' }),
-			() => play({ id: 'zelda' }),
-		];
-
-		setPlayAllSounds(playAllSounds);
-		setPlayerSounds(playerSounds);
-
-		//gameSoundRef.current = {playAllSounds, playerSounds}
-	}, []);
-	return { playAllSounds, playerSounds };
-};
+export * from "./useGameSounds";
