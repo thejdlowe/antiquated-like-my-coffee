49473971f34a6b19683c9d6fa6b1148b13bc8c0e
J.D. Lowe <jdlowe.dev@gmail.com>
2024-06-07 22:58:39 -0400

    Individual sounds should work

--- src/renderer/pages/GameScreen/components/PlayerColumn.tsx
+++ src/renderer/pages/GameScreen/components/PlayerColumn.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Grid, Typography } from '@mui/material';
 import useSound from 'use-sound';
 export const PlayerColumn = ({
@@ -20,40 +20,63 @@ export const PlayerColumn = ({
 	setActivePlayer: any;
 	playSound: number;
 }) => {
-	const [play] = useSound('./LMCsoundeffects.mp3', {
-		sprite: {
-			amongus: [0, 3356],
-			fortnite: [6468, 6557 - 6468],
-			gameboy: [7556, 8962 - 7556],
-			homerun: [10275, 11372 - 10275],
-			icq: [12948, 14081 - 12948],
-			mariopause: [17188, 18185 - 17188],
-			metalgear: [19859, 21583 - 19859],
-			mariopowerup: [22427, 23697 - 22427],
-			konami: [24445, 25216 - 24445],
-			wheeloffortune: [26008, 27233 - 26008],
-			zelda: [29336, 31150 - 29336],
-		},
-	});
-	const playerSounds = [
-		() => play({ id: 'amongus' }),
-		() => play({ id: 'fortnite' }),
-		() => play({ id: 'gameboy' }),
-		() => play({ id: 'homerun' }),
-		() => play({ id: 'icq' }),
-		() => play({ id: 'mariopause' }),
-		() => play({ id: 'metalgear' }),
-		() => play({ id: 'mariopowerup' }),
-		() => play({ id: 'konami' }),
-		() => play({ id: 'wheeloffortune' }),
-		() => play({ id: 'zelda' }),
+	const files: string[] = [
+		'./amongus.mp3',
+		'./fortnite.mp3',
+		'./gameboystartup.mp3',
+		'./homerun.mp3',
+		'./icq.mp3',
+		'./KonamiPause1.mp3',
+		'./mariopause.mp3',
+		'./mgs.mp3',
+		'./nsmb_power-up.mp3',
+		'./zelda.mp3',
+		'./wof.mp3',
 	];
+	const file = files[playSound] ?? '';
+	const audioElement = new Audio(file);
+	const myAudioRef = useRef(audioElement);
 	useEffect(() => {
 		if (isActive) {
-			const sound = playerSounds[playSound];
-			if (sound) sound();
+			if (myAudioRef.current) {
+				myAudioRef.current.play();
+			}
 		}
-	}, [play, isActive]);
+	}, [isActive, myAudioRef]);
+	// const [play] = useSound('./LMCsoundeffects.mp3', {
+	// 	sprite: {
+	// 		amongus: [0, 3356],
+	// 		fortnite: [6468, 6557 - 6468],
+	// 		gameboy: [7556, 8962 - 7556],
+	// 		homerun: [10275, 11372 - 10275],
+	// 		icq: [12948, 14081 - 12948],
+	// 		mariopause: [17188, 18185 - 17188],
+	// 		metalgear: [19859, 21583 - 19859],
+	// 		mariopowerup: [22427, 23697 - 22427],
+	// 		konami: [24445, 25216 - 24445],
+	// 		wheeloffortune: [26008, 27233 - 26008],
+	// 		zelda: [29336, 31150 - 29336],
+	// 	},
+	// });
+	// const playerSounds = [
+	// 	() => play({ id: 'amongus' }),
+	// 	() => play({ id: 'fortnite' }),
+	// 	() => play({ id: 'gameboy' }),
+	// 	() => play({ id: 'homerun' }),
+	// 	() => play({ id: 'icq' }),
+	// 	() => play({ id: 'mariopause' }),
+	// 	() => play({ id: 'metalgear' }),
+	// 	() => play({ id: 'mariopowerup' }),
+	// 	() => play({ id: 'konami' }),
+	// 	() => play({ id: 'wheeloffortune' }),
+	// 	() => play({ id: 'zelda' }),
+	// ];
+	// useEffect(() => {
+	// 	if (isActive) {
+	// 		const sound = playerSounds[playSound];
+	// 		if (sound) sound();
+	// 	}
+	// }, [play, isActive]);
 	return (
 		<Grid
 			container
