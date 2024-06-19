18b1b0eac9e6baf2d77584dac0c177ce6c66f9be
J.D. Lowe <jdlowe.dev@gmail.com>
2024-06-19 19:45:15 -0400

    Work damn you

--- src/renderer/pages/GameScreen/components/PlayerColumn.tsx
+++ src/renderer/pages/GameScreen/components/PlayerColumn.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Grid, Typography } from '@mui/material';
 import { audioData } from '../../../media';
 import './playerColumn.css';
+
 export const PlayerColumn = ({
 	playerName,
 	pronouns,
